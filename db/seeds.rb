c1bd0f353a43181fdb491aaa58626467876c0d98
fernando chaim <fmchaim@gmail.com>
2024-01-18 21:02:47 -0300

    3 mais ou menos ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+Flat.destroy_all # Optional: Clear the flats table before seeding
+
+4.times do
+  Flat.create!(
+    name: Faker::Address.community,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    price_per_night: Faker::Number.between(from: 50, to: 200),
+    number_of_guests: Faker::Number.between(from: 1, to: 5)
+  )
+end
